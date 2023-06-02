3c956c160e3a11ac5f2601005826a13e556b9a08
jefmth <96843399+jefmth@users.noreply.github.com>
2023-06-02 11:47:26 -0300

    Add files via upload

---
+++ senha.js
@@ -0,0 +1,20 @@
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Verificar usuário e senha
+    if (username === "mclove" && password === "4zero") {
+        // Salvar o nome de usuário no armazenamento local
+        localStorage.setItem("username", username);
+        
+        window.location.href = "galeria.html"; // Redirecionar para a página galeria.html
+    } else {
+        alert("Usuário ou senha inválidos. Por favor, tente novamente.");
+    }
+}
+
+function voltar() {
+    window.location.href = "index.html"; // Redirecionar para a página index.html
+}
+
+
