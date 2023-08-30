77a82d3cad0eced9651f48ddc3fd944615d0acbb
jefmth <96843399+jefmth@users.noreply.github.com>
2023-08-30 16:16:03 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,68 @@
+ function myFunction01() {
+             document.getElementById("overlay").style.display = "block";
+            document.getElementById("loader").style.display = "block";
+            document.getElementById("loading-text").style.display = "block";
+            setTimeout(function(){ 
+                document.getElementById("overlay").style.display = "none";
+                document.getElementById("loader").style.display = "none"; 
+                document.getElementById("loading-text").style.display = "none"; 
+                window.location.href = 'escalas.html';
+            }, 2000);
+        }
+ function myFunction02() {
+            document.getElementById("overlay").style.display = "block";
+            document.getElementById("loader").style.display = "block";
+            document.getElementById("loading-text").style.display = "block";
+            setTimeout(function(){ 
+                document.getElementById("overlay").style.display = "none";
+                document.getElementById("loader").style.display = "none"; 
+                document.getElementById("loading-text").style.display = "none";
+                window.location.href = 'galeria.html';
+            }, 2000);
+        }
+ function myFunction03() {
+             document.getElementById("overlay").style.display = "block";
+            document.getElementById("loader").style.display = "block";
+            document.getElementById("loading-text").style.display = "block";
+            setTimeout(function(){ 
+                document.getElementById("overlay").style.display = "none";
+                document.getElementById("loader").style.display = "none"; 
+                document.getElementById("loading-text").style.display = "none"; 
+                window.location.href = 'menu_agente.html';
+            }, 2000);
+        }
+//imposto de renda não prescisa
+ function myFunction04() {
+            document.getElementById("overlay").style.display = "block";
+            document.getElementById("loader").style.display = "block";
+            document.getElementById("loading-text").style.display = "block";
+            setTimeout(function(){ 
+                document.getElementById("overlay").style.display = "none";
+                document.getElementById("loader").style.display = "none"; 
+                document.getElementById("loading-text").style.display = "none";
+                window.location.href = 'galeria.html';
+            }, 2000);
+        }
+//fim imposto
+ function myFunction05() {
+             document.getElementById("overlay").style.display = "block";
+            document.getElementById("loader").style.display = "block";
+            document.getElementById("loading-text").style.display = "block";
+            setTimeout(function(){ 
+                document.getElementById("overlay").style.display = "none";
+                document.getElementById("loader").style.display = "none"; 
+                document.getElementById("loading-text").style.display = "none";
+                window.location.href = 'reciclagem.html';
+            }, 2000);
+        }
+ function myFunction06() {
+             document.getElementById("overlay").style.display = "block";
+            document.getElementById("loader").style.display = "block";
+            document.getElementById("loading-text").style.display = "block";
+            setTimeout(function(){ 
+                document.getElementById("overlay").style.display = "none";
+                document.getElementById("loader").style.display = "none"; 
+                document.getElementById("loading-text").style.display = "none";
+                window.location.href = 'carta.html';
+            }, 2000);
+        }
